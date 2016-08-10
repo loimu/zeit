0bcfd494e163397dd35ff6f472bf4fc867cbdd6a
blaze <blaze@vivaldi.net>
2016-08-10 14:58:57 +0300

    zeit: check if user is root

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,41 @@
-#include "mainwindow.h"
+/* ========================================================================
+*    Copyright (C) 2015-2016 Blaze <blaze@vivaldi.net>
+*
+*    This file is part of Zeit.
+*
+*    Zeit is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    Zeit is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with Zeit.  If not, see <http://www.gnu.org/licenses/>.
+* ======================================================================== */
+
+#include <unistd.h>
 #include <QApplication>
 #include <QTranslator>
 
+#include "mainwindow.h"
+
 int main(int argc, char *argv[])
 {
+    if(getuid() == 0)
+        qFatal("User should not be root");
     QApplication a(argc, argv);
     QApplication::setApplicationName("Zeit");
     QApplication::setOrganizationName("zeit");
     QApplication::setApplicationVersion("0.1");
-
     QTranslator translator;
     translator.load(QApplication::applicationDirPath() +
                     "/../share/zeit/translations/" + QLocale::system().name() + ".qm");
     a.installTranslator(&translator);
-
     MainWindow w;
     w.show();
-
     return a.exec();
 }
