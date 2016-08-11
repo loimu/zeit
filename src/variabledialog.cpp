650a124651bd9aa19459c1bb277e7824f9ecb894
blaze <blaze@vivaldi.net>
2016-08-11 20:24:46 +0300

    zeit: added VariableDialog

---
+++ src/variabledialog.cpp
@@ -0,0 +1,57 @@
+/* ========================================================================
+*    Copyright (C) 2015-2016 Blaze <blaze@vivaldi.net>
+*
+*    This file is part of Zeit.
+*
+*    Zeit is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    Zeit is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with Zeit.  If not, see <http://www.gnu.org/licenses/>.
+* ======================================================================== */
+
+#include "ctvariable.h"
+#include "variabledialog.h"
+#include "ui_variabledialog.h"
+
+VariableDialog::VariableDialog(CTVariable* _ctVar, const QString& _caption, QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::VariableDialog),
+    variable(_ctVar)
+{
+    ui->setupUi(this);
+    setWindowTitle(_caption);
+    // ButtonBox action
+    connect(ui->varEdit, SIGNAL(textChanged(QString)), SLOT(validate()));
+    connect(ui->valEdit, SIGNAL(textChanged(QString)), SLOT(validate()));
+    connect(this, SIGNAL(accepted()), SLOT(saveVariable()));
+    ui->buttonBox->setDisabled(true);
+}
+
+VariableDialog::~VariableDialog()
+{
+    delete ui;
+}
+
+void VariableDialog::validate() {
+    if(ui->varEdit->text().isEmpty() || ui->valEdit->text().isEmpty()) {
+        ui->errorLabel->setText("<b>fill both var and value</b>");
+        ui->buttonBox->setDisabled(true);
+    } else {
+        ui->errorLabel->clear();
+        ui->buttonBox->setEnabled(true);
+    }
+}
+
+void VariableDialog::saveVariable() {
+    variable->variable = ui->varEdit->text();
+    variable->value = ui->valEdit->text();
+    variable->comment = ui->commentEdit->text();
+}
---
+++ src/variabledialog.h
@@ -0,0 +1,47 @@
+/* ========================================================================
+*    Copyright (C) 2015-2016 Blaze <blaze@vivaldi.net>
+*
+*    This file is part of Zeit.
+*
+*    Zeit is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    Zeit is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with Zeit.  If not, see <http://www.gnu.org/licenses/>.
+* ======================================================================== */
+
+#ifndef VARIABLEDIALOG_H
+#define VARIABLEDIALOG_H
+
+#include <QDialog>
+
+namespace Ui {
+class VariableDialog;
+}
+
+class CTVariable;
+
+class VariableDialog : public QDialog
+{
+    Q_OBJECT
+
+    Ui::VariableDialog* ui;
+    CTVariable* variable;
+
+public:
+    explicit VariableDialog(CTVariable*, const QString&, QWidget *parent = nullptr);
+    ~VariableDialog();
+
+private slots:
+    void validate();
+    void saveVariable();
+};
+
+#endif // VARIABLEDIALOG_H
