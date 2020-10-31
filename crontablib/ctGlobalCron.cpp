6059419d19772f7aa2f432127f6edd76a6c96c80
blaze <blaze@vivaldi.net>
2020-10-31 11:28:21 +0300

    crontablib: in CTGlobalCron replace tabs with spaces

--- crontablib/ctGlobalCron.cpp
+++ crontablib/ctGlobalCron.cpp
@@ -16,18 +16,18 @@
 
 #include "logging.h"
 
-CTGlobalCron::CTGlobalCron(CTHost* _ctHost) :
-	CTCron() {
-		
+
+CTGlobalCron::CTGlobalCron(CTHost* _ctHost) : CTCron()
+{
     logDebug() << "Initializing CTGlobalCron";
-	
-	d->multiUserCron = true;
-	d->systemCron = false;
-	d->currentUserCron = false;
+
+    d->multiUserCron = true;
+    d->systemCron = false;
+    d->currentUserCron = false;
 
     d->userLogin = tr("All users");
-		
-	ctHost = _ctHost;
+
+    ctHost = _ctHost;
 }
 
 CTGlobalCron::~CTGlobalCron() {
@@ -36,99 +36,98 @@ CTGlobalCron::~CTGlobalCron() {
 
 const QList<CTTask*> CTGlobalCron::tasks() const {
     logDebug() << "Global Cron Tasks";
-	QList<CTTask*> tasks;
-	
+    QList<CTTask*> tasks;
+
     for(CTCron* cron: ctHost->cronInstances) {
-		if (cron->isSystemCron())
-			continue;
-		
+        if (cron->isSystemCron())
+            continue;
+
         for(CTTask* task: cron->tasks()) {
-			tasks.append(task);
-		}
-	}
-	return tasks;
+            tasks.append(task);
+        }
+    }
+    return tasks;
 }
 
 const QList<CTVariable*> CTGlobalCron::variables() const {
     logDebug() << "Global Cron Variables";
-	QList<CTVariable*> variables;
-	
+    QList<CTVariable*> variables;
+
     for(CTCron* cron: ctHost->cronInstances) {
-		if (cron->isSystemCron())
-			continue;
-		
+        if (cron->isSystemCron())
+            continue;
+
         for(CTVariable* variable: cron->variables()) {
-			variables.append(variable);
-		}
-	}
-	
-	return variables;
+            variables.append(variable);
+        }
+    }
+
+    return variables;
 }
 
 void CTGlobalCron::addTask(CTTask* task) {
     logDebug() << "Global Cron addTask";
-	
-	CTCron* actualCron = ctHost->findUserCron(task->userLogin);
-	actualCron->addTask(task);
+
+    CTCron* actualCron = ctHost->findUserCron(task->userLogin);
+    actualCron->addTask(task);
 }
 
 void CTGlobalCron::addVariable(CTVariable* variable) {
     logDebug() << "Global Cron addVariable";
 
-	CTCron* actualCron = ctHost->findUserCron(variable->userLogin);
-	actualCron->addVariable(variable);
+    CTCron* actualCron = ctHost->findUserCron(variable->userLogin);
+    actualCron->addVariable(variable);
 }
 
 
 void CTGlobalCron::modifyTask(CTTask* task) {
     logDebug() << "Global Cron modifyTask";
-	
-	CTCron* actualCron = ctHost->findCronContaining(task);
-	
-	/*
-	 * actualCron could be NULL is the task came from clipboard because those tasks are never
-	 * linked to an existing CTCron* object
-	 */ 
+
+    CTCron* actualCron = ctHost->findCronContaining(task);
+
+    /*
+     * actualCron could be NULL is the task came from clipboard because those tasks are never
+     * linked to an existing CTCron* object
+     */
     if (actualCron == nullptr || actualCron->userLogin() != task->userLogin) {
         if (actualCron!=nullptr) {
-			actualCron->removeTask(task);
-		}
-		
-		CTCron* newCron = ctHost->findUserCron(task->userLogin);
-		newCron->addTask(task);
-	}
+            actualCron->removeTask(task);
+        }
+
+        CTCron* newCron = ctHost->findUserCron(task->userLogin);
+        newCron->addTask(task);
+    }
 }
 
 void CTGlobalCron::modifyVariable(CTVariable* variable) {
     logDebug() << "Global Cron modifyVariable";
-	
-	CTCron* actualCron = ctHost->findCronContaining(variable);
-	
-	/*
-	 * actualCron could be NULL is the task came from clipboard because those tasks are never
-	 * linked to an existing CTCron* object
-	 */ 
+
+    CTCron* actualCron = ctHost->findCronContaining(variable);
+
+    /*
+     * actualCron could be NULL is the task came from clipboard because those tasks are never
+     * linked to an existing CTCron* object
+     */
     if (actualCron == nullptr || actualCron->userLogin() != variable->userLogin) {
         if (actualCron!=nullptr) {
-			actualCron->removeVariable(variable);
-		}
-		
-		CTCron* newCron = ctHost->findUserCron(variable->userLogin);
-		newCron->addVariable(variable);
-	}
+            actualCron->removeVariable(variable);
+        }
+
+        CTCron* newCron = ctHost->findUserCron(variable->userLogin);
+        newCron->addVariable(variable);
+    }
 }
 
 void CTGlobalCron::removeTask(CTTask* task) {
     logDebug() << "Global Cron removeTask";
-	
-	CTCron* actualCron = ctHost->findCronContaining(task);
-	actualCron->removeTask(task);
+
+    CTCron* actualCron = ctHost->findCronContaining(task);
+    actualCron->removeTask(task);
 }
 
 void CTGlobalCron::removeVariable(CTVariable* variable) {
     logDebug() << "Global Cron removeVariable";
-	
-	CTCron* actualCron = ctHost->findCronContaining(variable);
-	actualCron->removeVariable(variable);
-}
 
+    CTCron* actualCron = ctHost->findCronContaining(variable);
+    actualCron->removeVariable(variable);
+}
